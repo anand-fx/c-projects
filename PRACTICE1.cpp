8072abb5518842eea7ad4b2f3d96cc11ead9ced4
Pandya Anand <anandpandyapc999@gmail.com>
2026-05-03 14:02:21 +0530

    Add files via upload

---
+++ PRACTICE1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+auto itemPrice = 999 ;
+auto shippingcost = 200;
+
+auto sum = itemPrice + shippingcost;
+
+cout << sum ;
+return 0;
+
+}
