75b2432cadb52ce76c93ab82aeea8339d7c93164
Pandya Anand <anandpandyapc999@gmail.com>
2026-05-04 21:45:58 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,35 @@
+/*#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+string firstname;
+string lastname;
+cout << "your first name : ";
+cin >> firstname;
+cout << "your last name : ";
+cin >> lastname;
+cout << "your fullname is: " << firstname + lastname;
+return 0;
+}*/
+
+
+
+/*#include <iostream>
+#include <string>
+
+int main()
+{
+    std :: string greeting = "hello";
+    std :: cout << greeting;
+    return 0;
+
+}*/
+
+
+
+#include <iostream>
+#include <string>
+#include <cmath>
