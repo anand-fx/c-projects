15bdc6847a372ea9ae6d3ea5ef719b495994cbdd
Pandya Anand <anandpandyapc999@gmail.com>
2026-05-13 12:16:22 +0530

    Implement car class and display car details

--- practice.cpp
+++ practice.cpp
@@ -29,7 +29,30 @@ int main()
 }*/
 
 
-
 #include <iostream>
 #include <string>
-#include <cmath>
+
+using namespace std;
+
+class car {
+public:
+    string model;
+    string brand;
+    int year;
+    };
+
+    int main(){
+    car carobj1;
+    carobj1.model = "BMW";
+    carobj1.brand = "x5";
+    carobj1.year = 2009;
+
+    car carobj2;
+    carobj2.model = "benz";
+    carobj2.brand = "v12";
+    carobj2.year = 2011;
+
+    cout << carobj1.model << " " << carobj1.brand << " " << carobj1.year << "\n";
+    cout << carobj2.model << " " << carobj2.brand << " " << carobj2.year << "\n";
+    return 0;
+    }
